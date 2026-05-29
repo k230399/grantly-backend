2f8ef26c4c7b0103d01fd33b16ac05ac76e3a111
Isaiah Quindo <k230399@student.kent.edu.au>
2026-05-29 18:55:36 +1000

    Import Request class in AuthController for improved request handling.

--- app/Http/Controllers/Api/V1/AuthController.php
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\Auth\LoginRequest;
 use App\Http\Requests\Auth\RegisterRequest;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 
 class AuthController extends Controller
